1fed55d86b0a09f333d9a36dbc329b9db61c6072
madhu1319s <dmadhu1861999@gmail.com>
2023-12-11 19:37:41 +0530

    day9

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import CarouselProduct from "./carousel/carousel";
+import Timer from "./timer/timer";
+
+function App() {
+  return (
+    <>
+    <CarouselProduct/>
+    <Timer/>
+    </>
+  );
+}
+
+export default App;
